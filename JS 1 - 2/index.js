618d97f5ed5005b51dffde9791465aaeec8edcca
SyedaManiaFaraz <syedamaniafaraz@gmail.com>
2024-10-01 00:11:36 +0500

    Add files via upload

---
+++ JS 1 - 2/index.js
@@ -0,0 +1,52 @@
+                         // CHAPTER NUMBER 01
+
+
+// QUESTION NO 1   
+The alert() function in JavaScript is used to display a popup dialog box with a specified message.      
+
+// // QUESTION NO 2
+alert("Welcome to my Website!");
+
+// // QUESTION NO 3
+If you Capitalize the "alert" then javascripts gives an error because javascript is case sensitive and it must be written in lowercase letter.
+
+// QUESTION NO 4
+alert("Hello,world!")
+
+// QUESTION NO 5
+alert("MANIA HERE");
+alert       : This us built-in javascript function .
+(and)       : These round brackets are used to enclose the message .
+"MANIA HERE": This is the message which we display in the alert . 
+;           : Semicolon is placed in the end of the statement .
+
+
+
+
+                         // CHAPTER NUMBER 02
+
+  
+// QUESTION NO 1              
+var message = "Hello,world!";
+alert(message);
+
+// QUESTION NO 2
+var age;
+age = 25;
+alert(age);
+
+// QUESTION NO 3
+var greeting = "Good morning!";
+alert(greeting);
+
+greeting = "Good evening!";
+alert(greeting);
+
+// QUESTION NO 4
+var favoriteColor = "blue";
+alert("My favorite color is" + favoriteColor );
+ 
+// QUESTION NO 5
+var age;
+age = 25;
+alert(age);
